3b1ede5a246a8299fac84c757a84b8504fddcfcf
choyondas <choyondas08@gmail.com>
2021-12-19 15:34:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dassland watch store
+### This is an online watch store, from where people can easily buy their watches.
+#### main services of this shop are:
+- All users can visit our website without login or register. They also can see our available products.
+- If they want to know more about our product they have to  register.
+- All the information will be stored on database.
+- there has two parts one is for admin and other is for general users.
+- In the home page 7 sections for example(Header, Top Banner,Popular Watches,Our designer etc  )
+- Admin can make another user as admin.
+- Admin can see some special section named manage all orders, add products etc. what normal users can't see.
+
+This is live site link [Dassland watch](https://doctors-portal-c9ad8.web.app/)
+
+
+
+![front view of our website](https://i.ibb.co/Wf0cZPc/dassland-watch2.png)
+##### This is the front view of my website
---
+++ index.js
@@ -0,0 +1,217 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+const ObjectId = require("mongodb").ObjectID;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ryo5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("onlineshop");
+
+    const usersCollection = database.collection("users");
+
+    const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+    const reviewCollection = database.collection("review");
+
+    //post products
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      console.log(result);
+      res.json(result);
+    });
+
+    //get all products form databse
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    //get single product collection
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // delete product from database
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      console.log("deleted id", result);
+      res.json(result);
+    });
+
+    // //for users post
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+      console.log(result);
+    });
+
+    //put users
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    //special users get
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // post order info to orders collection
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      console.log(result);
+      res.json(result);
+    });
+
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      console.log("put", user);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    //get users orders from database
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = ordersCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+    // dleted user for my order review page btn
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      console.log("deleted id", result);
+      res.json(result);
+    });
+
+    // dleted user for my order review page btn
+    app.delete("/orders/:email", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      console.log("deleted id", result);
+      res.json(result);
+    });
+
+    //post review data
+
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      console.log(result);
+      res.json(result);
+    });
+    //get review data
+
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find();
+      const review = await cursor.toArray();
+      res.json(review);
+    });
+
+    // ---------manage All orders------
+
+    // get customers order in the my review page
+    app.get("/orders/all", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    // dleted users order from manage all order page
+    app.delete("/orders/all/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      console.log("deleted id", result);
+      res.json(result);
+    });
+
+    // user update from server to show
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.findOne(query);
+      res.send(result);
+      // ai part er pore data ta /5000/users/id te pabo
+    });
+
+    // show updated data after update by put
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          username: updatedUser.username,
+          address: updatedUser.address,
+        },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log("updating", id);
+      res.json(result);
+    });
+  } finally {
+    //await client.close();
+  }
+}
+
+run().catch(console.error);
+
+app.get("/", (req, res) => {
+  res.send("online ecommerce shop");
+});
+
+app.listen(port, () => {
+  console.log(`listening port: ${port}`);
+});
